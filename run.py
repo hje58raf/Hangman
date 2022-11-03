12573fcc2394445829dafe7df9ced3067e80c667
hje58raf <mjelavic@inet.hr>
2022-11-03 09:28:07 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,17 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+import random
+
+words = ['chicken', 'dog', 'cat', 'mouse', 'frog']
+
+lives_remaining = 14
+guessed_letters = ‘’
+
+# Getting a Random Word
+def pick_a_word():
+word_position = random.randint(0, len(words) - 1)
+return words[word_position]
+print(pick_a_word())
+
